382f6f95adea242051a13f9ea85bdb7da120870e
Macro-internshala <piyushharde100@gmail.com>
2025-10-10 15:17:50 +0530

    ui changes in livetrack

--- src/components/dashboard/LiveTrack.tsx/single-device-livetrack-controls.tsx
+++ src/components/dashboard/LiveTrack.tsx/single-device-livetrack-controls.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { VehicleData } from "./SingleDeviceLiveTrack";
+import Image from "next/image";
+import { Satellite } from "lucide-react";
 
 interface SingleDeviceLiveTrackControlsProps {
   vehicle: VehicleData | null;
@@ -70,19 +72,7 @@ export const SingleDeviceLiveTrackControls: React.FC<
         }`}
         title={isSatelliteView ? "Map View" : "Satellite View"}
       >
-        <svg
-          className="w-6 h-6"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
+        <Satellite />
       </button>
 
       {/* Traffic Toggle Button */}
@@ -91,11 +81,11 @@ export const SingleDeviceLiveTrackControls: React.FC<
         className={`p-3 rounded-lg shadow-lg transition-all duration-200 ${
           showTraffic
             ? "bg-orange-500 text-white hover:bg-orange-600"
-            : "bg-white text-gray-700 hover:bg-gray-100"
+            : "bg-gray-700  hover:bg-gray-900"
         }`}
         title={showTraffic ? "Hide Traffic" : "Show Traffic"}
       >
-        <svg
+        {/* <svg
           className="w-6 h-6"
           fill="none"
           stroke="currentColor"
@@ -107,7 +97,12 @@ export const SingleDeviceLiveTrackControls: React.FC<
             strokeWidth={2}
             d="M13 10V3L4 14h7v7l9-11h-7z"
           />
-        </svg>
+        </svg> */}
+        <img
+          src={"/icons/traffic.svg"}
+          alt="traffic view"
+          className="reverse"
+        />
       </button>
 
       {/* Show/Hide Geofences Button */}
